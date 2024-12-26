e2cc7a0837779dee4782fac913e26879a50b8b8b
Kerry-Mannette <kerrymannette2@gmail.com>
2024-12-26 16:03:18 -0400

    Add files via upload

---
+++ js/navigation.js
@@ -0,0 +1,16 @@
+function myFunction(n){
+    if(n == 1){
+     location.href = "#home" 
+    }else if(n == 2){
+      location.href = "#about" 
+    }else if (n == 3){
+      location.href = "#services"
+    }else if (n == 4){
+      location.href = "#courses"
+    }else if (n == 5){
+      location.href = "#contact"
+    }else if (n == 6){
+      location.href = "#popup2"
+    }
+    
+  }
